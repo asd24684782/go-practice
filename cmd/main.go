d83db49de29b8b7b23fa60f650d61162bf4b0481
asd24684782 <asd24684782@gmail.com>
2022-06-16 23:27:41 +0800

    book

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-practice/pkg/books"
+	"go-practice/pkg/common/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+    viper.SetConfigFile("./pkg/common/envs/.env")
+    viper.ReadInConfig()
+
+    port := viper.Get("PORT").(string)
+    dbUrl := viper.Get("DB_URL").(string)
+
+    r := gin.Default()
+    h := db.Init(dbUrl)
+
+    books.RegisterRoutes(r, h)
+    // register more routes here
+
+    r.Run(port)
+}
